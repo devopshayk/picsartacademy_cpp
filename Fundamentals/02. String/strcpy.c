6076d4126099bf518a15b2c0222a0c7f94c0344e
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-12 21:05:06 +0400

    last-updates

---
+++ Fundamentals/02. String/strcpy.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h> 
+
+	char * strCpy(const char * str1, char * str2);
+
+	int main() {
+		char str1[50];
+		char str2[50];
+		
+		fputs("Write text for Str1: ", stdout);
+		
+		fgets(str1, 50, stdin);
+		str1[strcspn(str1, "\n")] = '\0'; // For deleted '\n' symbol		
+
+		char * result = strCpy(str1, str2);
+
+		fprintf(stdout, "Str2 value = %s\n", result);
+
+		return 0;
+	}
+
+	char * strCpy(const char * str1, char * str2) {
+		int index = 0;
+
+		while(str1[index] != '\0') {
+			str2[index] = str1[index];
+			index++;
+		}
+		
+		str2[index] = '\0';
+
+		return str2;
+	}
