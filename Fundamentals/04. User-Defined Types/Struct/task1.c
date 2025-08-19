8b9dc128f551cd74b08164dbb09bb2747d9e4082
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-19 17:22:17 +0400

    last-updates

---
+++ Fundamentals/04. User-Defined Types/Struct/task1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+	typedef struct {
+		int length;
+		int width;
+	} Rectangle;
+
+	
+	int main() {
+		int area = 0, perimeter = 0;
+ 
+		Rectangle r;
+
+
+		printf("\tWrite parameters to calculate the area of a rectanglei\n");
+
+		puts("");
+
+		printf("Write length for rectangle: ");
+		scanf("%d", &r.length);
+	
+		printf("Write width for rectangle: ");
+		scanf("%d", &r.width);
+
+		area = r.length * r.width;
+
+		puts("");
+
+
+
+		printf("\tWrite parameters to calculate the perimeter of a rectangle\n");
+
+		puts("");
+
+                printf("Write length for rectangle: ");
+                scanf("%d", &r.length);
+
+                printf("Write width for rectangle: ");
+                scanf("%d", &r.width);
+
+                perimeter = 2 * (r.length + r.width);
+
+		puts("");
+
+		printf("Area of rectangle = %d\n", area);
+		printf("Perimeter of rectangle = %d\n", perimeter);
+	}
