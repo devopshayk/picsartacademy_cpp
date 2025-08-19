512dff642f7592e57731a48cec9c640d897e9cdb
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-19 22:26:37 +0400

    last-updates

--- Fundamentals/04. User-Defined Types/Union/task4.c
+++ Fundamentals/04. User-Defined Types/Union/task4.c
@@ -16,17 +16,17 @@
 			printf("Choice: ");
     		scanf("%d", &choice);
 
-    		if(choice == 1) {
+    		if (choice == 1) {
         		printf("Enter length: ");
         		scanf("%d", &m.length);
         		printf("\nStored length = %d\n", m.length);
-    		} else if(choice == 2) {
+    		} else if (choice == 2) {
         		printf("Enter weight: ");
         		scanf("%f", &m.weight);
         		printf("\nStored weight = %.2f\n", m.weight);
-    		} else {
+    		} else 
         		printf("Invalid choice!\n");
-    		}
+    		
 
     		return 0;
 	}
