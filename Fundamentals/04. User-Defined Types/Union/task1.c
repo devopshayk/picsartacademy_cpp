17ef6830125a29dcc92f55b0b8501e1394bd759b
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-19 21:08:26 +0400

    last-updates

--- Fundamentals/04. User-Defined Types/Union/task1.c
+++ Fundamentals/04. User-Defined Types/Union/task1.c
@@ -11,13 +11,13 @@
     		printf("Enter an integer: ");
     		scanf("%d", &num.intValue);
     
-		printf("As int: %d\n", num.intValue);
+			printf("As int: %d\n", num.intValue);
     		printf("As float: %f\n", num.floatValue);
 
     		printf("\nEnter a float: ");
     		scanf("%f", &num.floatValue);
     
-		printf("As float: %f\n", num.floatValue);
+			printf("As float: %f\n", num.floatValue);
     		printf("As int: %d\n", num.intValue);
 
     		return 0;
