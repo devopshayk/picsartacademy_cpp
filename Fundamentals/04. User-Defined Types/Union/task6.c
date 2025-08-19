60233a1b86e711185641c2bb6c29b0b9098b8924
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-19 22:41:41 +0400

    last-updates

---
+++ Fundamentals/04. User-Defined Types/Union/task6.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+	typedef union {
+    		struct {
+        		int hours;
+        		int minutes;
+    		} hms;
+    		
+		float totalHours;
+	} Time;
+
+	int main() {
+    		Time t;
+    		int choice;
+
+    		printf("\tChoose input format:\n\n");
+    		printf("1. Hours and minutes\n");
+    		printf("2. Total hours (float)\n");
+    
+			printf("Choice: ");
+    		scanf("%d", &choice);
+
+    		if (choice == 1) {
+        		printf("Enter hours: ");
+        		scanf("%d", &t.hms.hours);
+        
+				printf("Enter minutes: ");
+        		scanf("%d", &t.hms.minutes);
+
+        		printf("\nYou entered: %d hours and %d minutes\n", t.hms.hours, t.hms.minutes);
+        		printf("Memory as float: %.2f\n", t.totalHours);
+    		}
+
+    		else if (choice == 2) {
+        		printf("Enter total hours (float): ");
+        		scanf("%f", &t.totalHours);
+
+        		printf("\nYou entered: %.2f total hours\n", t.totalHours);
+        		printf("Memory as hours and minutes: %d hours, %d minutes\n", t.hms.hours, t.hms.minutes);
+    		}
+    		
+		else
+        		printf("Invalid choice!\n");
+    		
+
+    		return 0;
+	}
+
