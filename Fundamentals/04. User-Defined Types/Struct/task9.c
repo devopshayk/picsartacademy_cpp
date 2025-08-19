3b3bb19ef516b796f40a5edf98cdb3d2524e0b8a
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-19 20:49:09 +0400

    last-updates

---
+++ Fundamentals/04. User-Defined Types/Struct/task9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+	typedef struct {
+    		int first;
+    		int second;
+	} Pair;
+
+	int main() {
+    		Pair p, temp;
+
+    		printf("\tEnter two integer values\n\n");
+
+    		printf("First value: ");
+    		scanf("%d", &p.first);
+    
+		printf("Second value: ");
+    		scanf("%d", &p.second);
+
+    		temp.first = p.first;
+    		temp.second = p.second;
+
+    		p.first = temp.second;
+    		p.second = temp.first;
+
+    		printf("\nAfter swapping:\n\n");
+    		printf("First value = %d\n", p.first);
+    		printf("Second value = %d\n", p.second);
+
+    		return 0;
+	}
+
