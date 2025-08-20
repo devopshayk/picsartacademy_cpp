b2266b6f03e2a1141c00ddb9dd11b1c49b7e45db
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-20 21:48:32 +0400

    last-updates

--- Fundamentals/06. Academy Management System/main.c
+++ Fundamentals/06. Academy Management System/main.c
@@ -16,8 +16,8 @@ int main() {
 	printStudentReport(&st[0]);
 	
 	int month1 = st[0].monthExam[0];
-    	int month2 = st[0].monthExam[1];
-    	int minMonth3 = minScoreForThirdMonth(month1, month2);
+    int month2 = st[0].monthExam[1];
+    int minMonth3 = minScoreForThirdMonth(month1, month2);
     
 	printf("Minimum score needed for third month to pass: %d\n", minMonth3);
 	
