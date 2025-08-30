553f2ebd5705af41636f88d79d9fa9366efc0576
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-30 17:40:58 +0400

    last-updates

---
+++ Fundamentals/08. Dynamic Memory/task2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+	int main() {
+    		char buffer[1000];
+    		char *str;
+
+    		printf("Write text: ");
+    		fgets(buffer, sizeof(buffer), stdin);
+    		buffer[strcspn(buffer, "\n")] = '\0';
+
+    		str = malloc(strlen(buffer) + 1);
+
+    		strcpy(str, buffer);
+
+    		printf("Text: \"%s\"\n", str);
+    		printf("Length: %zu\n", strlen(str));
+
+    		free(str);
+    		
+		return 0;
+	}
+
