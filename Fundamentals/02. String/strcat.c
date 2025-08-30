63fc1cbf906dc2238e958369018684209fd7b619
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-08-31 02:15:53 +0400

    last-updates

--- Fundamentals/02. String/strcat.c
+++ Fundamentals/02. String/strcat.c
@@ -1,38 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-	int main() {
-		int size = 0;
-
-		printf("Write size for int array: ");
-		scanf("%d", &size);
-
-		int *arr = (int *)malloc(size * sizeof(int));
-
-		printf("Write %d elements for array: ", size);
-		for(int i = 0; i < size; i++) {
-			scanf("%d", &arr[i]);
+	void* my_realloc(void* ptr, size_t old_size, size_t new_size) {
+		if (ptr == NULL)
+			return malloc(new_size);
+		
+		if (new_size == 0) {
+			free(ptr);
+			return NULL;
 		}
-
-		int new_elements = 0;
-		printf("Write number of new elements to add: ");
-		scanf("%d", &new_elements);
-
-		arr = (int *)realloc(arr, (size + new_elements) * sizeof(int));
-
-		printf("Write %d new elements: ", new_elements);
-		for(int i = size; i < size + new_elements; i++) {
-			scanf("%d", &arr[i]);
-		}
-
-		size += new_elements;
-
-		puts("");
-		printf("Array's elements:");
-		for(int i = 0; i < size; i++) printf(" %d", arr[i]);
-
-		puts("");
-		free(arr);
-
-		return 0;
+		
+		void* new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return NULL;
+		
+		size_t copy_size = old_size < new_size ? old_size : new_size;
+		memcpy(new_ptr, ptr, copy_size);
+		
+		free(ptr);
+		return new_ptr;
 	}
--- Fundamentals/08. Dynamic Memory/task6.c
+++ Fundamentals/08. Dynamic Memory/task6.c
@@ -2,26 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-    void* my_realloc(void* ptr, size_t new_size) {
-        if (ptr == NULL) {
-            return malloc(new_size);
-        }
-        
-        if (new_size == 0) {
-            free(ptr);
-            return NULL;
-        }
-        
-        void* new_ptr = malloc(new_size);
-        if (new_ptr == NULL) {
-            return NULL;
-        }
-        
-        size_t copy_size = new_size;
-        
-        memcpy(new_ptr, ptr, copy_size);
-        
-        free(ptr);
-        
-        return new_ptr;
-    }
+    	void* my_realloc(void* ptr, size_t old_size, size_t new_size) {
+		if (ptr == NULL)
+			return malloc(new_size);
+		
+		if (new_size == 0) {
+			free(ptr);
+			return NULL;
+		}
+		
+		void* new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return NULL;
+		
+		size_t copy_size = old_size < new_size ? old_size : new_size;
+		memcpy(new_ptr, ptr, copy_size);
+		
+		free(ptr);
+		return new_ptr;
+	}
