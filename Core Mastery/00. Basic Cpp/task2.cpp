f429920987310152e4563c1ce97485aef381336b
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-09-19 00:03:47 +0400

    last-updates

---
+++ Core Mastery/00. Basic Cpp/task2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+    void swap(int& x, int& y);
+
+    int main() {
+        int a, b;
+        
+        cout << "Write two nums for swap: ";
+        cin >> a >> b;
+
+        cout << "Num 1 = " << a << ", Num 2 = " << b << endl;
+
+        swap(a, b);
+
+        cout << "Num 1 = " << a << ", Num 2 = " << b << endl;
+
+        return 0;
+    }
+
+    void swap(int& x, int& y) {
+        int tmp = x;
+        x = y;
+        y = tmp;
+    }
