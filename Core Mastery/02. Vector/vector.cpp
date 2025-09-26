483092ab90d735b21234ecf963ba7fcb80820c69
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-09-27 01:05:06 +0400

    last-updates

--- Core Mastery/02. Vector/vector.cpp
+++ Core Mastery/02. Vector/vector.cpp
@@ -3,6 +3,7 @@
 
     void myvector_init(MyVector* v, size_t capacity) {
         v->capacity = capacity;
+        v->size = 0;
         v->data = new int[capacity];
     }
 
@@ -10,15 +11,15 @@
         v->capacity = v->size = size;
         v->data = new int[v->capacity];
 
-        for(int i = 0; i < size; ++i) v->data[i] = val;
+        for(size_t i = 0; i < size; ++i) v->data[i] = val;
     }
 
     void myvector_destroy(MyVector* v) {
         v->capacity = 0;
         v->size = 0;
-        v->data = nullptr;
-
         delete[] v->data;
+        
+        v->data = nullptr;
     }
 
     void myvector_clear(MyVector* v) {
@@ -39,12 +40,14 @@
 
     void myvector_push_back(MyVector* v, int value) {
         if(v->size == v->capacity) {
-            int * new_array = new int[v->capacity * 2];
+            size_t new_capacity = v->capacity * 2;
+            int * new_array = new int[new_capacity];
 
-            for (int i = 0; i < v->size; ++i) new_array[i] = v->data[i];
+            for (size_t i = 0; i < v->size; ++i) new_array[i] = v->data[i];
 
-            delete v->data;
+            delete[] v->data;
             v->data = new_array;
+            v->capacity = new_capacity;
             new_array = nullptr;
         }
 
@@ -53,24 +56,26 @@
     }
 
     void myvector_pop_back(MyVector* v) {
-        v->data[--v->size] = 0; 
+        if(v->size < 0) {
+            v->data[--v->size] = 0; 
+        }
     }
 
     void myvector_insert(MyVector* v, size_t index, int value) {
         if(index > v->size) return;
 
         if(v->size == v->capacity) {
-            int new_capacity = v->capacity = v->capacity * 2;
+            int new_capacity = v->capacity * 2;
 
             int* new_data = new int[new_capacity];
-            for(int i = 0; i < v->size; ++i) new_data[i] = v->data[i];
+            for(size_t i = 0; i < v->size; ++i) new_data[i] = v->data[i];
 
             delete[] v->data;
             v->data = new_data;
             v->capacity = new_capacity;
         }
 
-        for(int i = v->size; i > index; --i) v->data[i] = v->data[i - 1];
+        for(size_t i = v->size; i > index; --i) v->data[i] = v->data[i - 1];
 
         v->data[index] = value;
         v->size++;
@@ -79,11 +84,11 @@
     void myvector_erase(MyVector* v, size_t index) {
         if(index >= v->size) return;
 
-        for(int i = index; i < v->size - 1; i++) v->data[i] = v->data[i + 1];
+        for(size_t i = index; i < v->size - 1; i++) v->data[i] = v->data[i + 1];
 
         --v->size;
     }
 
     void myvector_print(const MyVector* v) {
-        for(int i = 0; i < v->size; i++) std::cout << v->data[i] << std::endl;
+        for(size_t i = 0; i < v->size; i++) std::cout << v->data[i] << std::endl;
     }
