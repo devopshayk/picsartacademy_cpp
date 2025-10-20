ca8eb4c2aaea8dfa3695645fcd17be793eefccf3
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2025-10-20 21:49:50 +0400

    last-updates

--- Core Mastery/02. Vector/vector.cpp
+++ Core Mastery/02. Vector/vector.cpp
@@ -2,94 +2,94 @@
 #include "vector.hpp"
 
 void myvector_init(MyVector* v, size_t capacity) {
-    v->capacity = capacity;
-    v->size = 0;
-    v->data = new int[capacity];
+    v -> capacity = capacity;
+    v -> size = 0;
+    v -> data = new int[capacity];
 }
 
 void myvector_init(MyVector* v, size_t size, int val) {
-    v->capacity = v->size = size;
-    v->data = new int[v->capacity];
+    v -> capacity = v -> size = size;
+    v -> data = new int[v -> capacity];
 
-    for(size_t i = 0; i < size; ++i) v->data[i] = val;
+    for(size_t i = 0; i < size; ++i) v -> data[i] = val;
 }
 
 void myvector_destroy(MyVector* v) {
-    v->capacity = 0;
-    v->size = 0;
-    delete[] v->data;
+    v -> capacity = 0;
+    v -> size = 0;
+    delete[] v -> data;
     
-    v->data = nullptr;
+    v -> data = nullptr;
 }
 
 void myvector_clear(MyVector* v) {
-    v->size = 0;
+    v -> size = 0;
 }
 
 size_t myvector_size(const MyVector* v) {
-    return v->size;
+    return v -> size;
 }
 
 size_t myvector_capacity(const MyVector* v) {
-    return v->capacity;
+    return v -> capacity;
 }
 
 void myvector_push_back(MyVector* v, int value) {
-    if(v->size == v->capacity) {
-        size_t new_capacity = v->capacity * 2;
+    if(v -> size == v -> capacity) {
+        size_t new_capacity = v -> capacity * 2;
         int * new_array = new int[new_capacity];
 
-        for (size_t i = 0; i < v->size; ++i) new_array[i] = v->data[i];
+        for (size_t i = 0; i < v -> size; ++i) new_array[i] = v -> data[i];
 
-        delete[] v->data;
-        v->data = new_array;
-        v->capacity = new_capacity;
+        delete[] v -> data;
+        v -> data = new_array;
+        v -> capacity = new_capacity;
         new_array = nullptr;
     }
 
-    v->data[v->size] = value;
-    v->size++;
+    v -> data[v -> size] = value;
+    v -> size++;
 }
 
 void myvector_pop_back(MyVector* v) {
-    if(v->size > 0) {
-        --v->size; 
+    if(v -> size > 0) {
+        --v -> size; 
     } else {
         std::cout << "Vector is empty" << std::endl;
     }
 }
 
 void myvector_insert(MyVector* v, size_t index, int value) {
-    if(index > v->size) return;
+    if(index > v -> size) return;
     if(index < 0) std::cout << "Enter positive index (0 >= index)" << std::endl;
 
-    if(v->size == v->capacity) {
-        int new_capacity = v->capacity * 2;
+    if(v -> size == v -> capacity) {
+        int new_capacity = v -> capacity * 2;
 
         int* new_data = new int[new_capacity];
-        for(size_t i = 0; i < v->size; ++i) new_data[i] = v->data[i];
+        for(size_t i = 0; i < v -> size; ++i) new_data[i] = v -> data[i];
         
-        delete[] v->data;
-        v->data = new_data;
-        v->capacity = new_capacity;
+        delete[] v -> data;
+        v -> data = new_data;
+        v -> capacity = new_capacity;
     }
 
-    for(size_t i = v->size; i > index; --i) v->data[i] = v->data[i - 1];
+    for(size_t i = v -> size; i > index; --i) v -> data[i] = v -> data[i - 1];
 
-    v->data[index] = value;
-    v->size++;
+    v -> data[index] = value;
+    v -> size++;
 }
 
 void myvector_erase(MyVector* v, size_t index) {
-    if(index >= v->size) return;
+    if(index >= v -> size) return;
     if(index < 0) std::cout << "Enter positive index (0 >= index)" << std::endl;
 
-    for(size_t i = index; i < v->size - 1; i++) v->data[i] = v->data[i + 1];
+    for(size_t i = index; i < v -> size - 1; i++) v -> data[i] = v -> data[i + 1];
 
-    --v->size;
+    --v -> size;
 }
 
 void myvector_print(const MyVector* v) {
-    for(size_t i = 0; i < v->size; i++) { std::cout <<  v->data[i] << " " ; }
+    for(size_t i = 0; i < v -> size; i++) { std::cout <<  v -> data[i] << " " ; }
     std::cout << std::endl;
 }
--- Core Mastery/03. CppToC/main.c
+++ Core Mastery/03. CppToC/main.c
@@ -30,18 +30,18 @@ void printInfoHeight(Person *, bool);
 
 
 void person_def_init(Person * p) {
-    strcpy(p->name, "Hayk");
-    p->age = 0;
-    p->height = 0.0; 
+    strcpy(p -> name, "Hayk");
+    p -> age = 0;
+    p -> height = 0.0; 
 }
 
 
 void person_param_init(Person * p, char * newName, int newAge, double newHeight) {
-    strncpy(p->name, newName, sizeof(p->name) - 1);
-    p->name[sizeof(p->name) - 1] = '\0';
+    strncpy(p -> name, newName, sizeof(p->name) - 1);
+    p -> name[sizeof(p -> name) - 1] = '\0';
 
-    p->age = newAge;
-    p->height = newHeight;
+    p -> age = newAge;
+    p -> height = newHeight;
 }
 
 
@@ -51,32 +51,32 @@ double getHeight(Person * p) { return p->height; }
 
 
 void setName(Person * p, const char * newName) {
-    strncpy(p->name, newName, sizeof(p->name) - 1);
-    p->name[sizeof(p->name) - 1] = '\0';
+    strncpy(p -> name, newName, sizeof(p -> name) - 1);
+    p -> name[sizeof(p -> name) - 1] = '\0';
 }
 
 
 void setAge(Person * p, int newAge) {
-    p->age = newAge;
+    p -> age = newAge;
 }
 
 
 void setHeight(Person * p, double newHeight) {
-    p->height = newHeight;
+    p -> height = newHeight;
 }
 
 
 void printInfo(Person *p) {
     printf("\tPerson's All Info\n\n");
 
-    printf("Name is: %s\n", p->name);
-    printf("Name is: %d\n", p->age);
-    printf("Name is: %.2f\n", p->height);
+    printf("Name is: %s\n", p -> name);
+    printf("Name is: %d\n", p -> age);
+    printf("Name is: %.2f\n", p -> height);
 }
 
 void printInfoHeight(Person * p, bool condition) {
     if(condition) {
-        printf("Person's Height = %.2f\n", p->height);
+        printf("Person's Height = %.2f\n", p -> height);
     } else {
         printf("Not valid condition for height: \n");
     }
