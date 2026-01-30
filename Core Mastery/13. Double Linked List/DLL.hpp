d7287037612a3b666016f61f19f512c6a4b19135
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2026-01-30 18:08:00 +0400

    last-updates

---
+++ Core Mastery/13. Double Linked List/DLL.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <initializer_list>
+
+
+template <typename T>
+class DLL {
+    private:
+        struct Node {
+            T value;
+            Node * next;
+            Node * prev;
+        };
+
+        Node * head;
+        Node * laid;
+
+    public:
+        DLL() = default;
+        DLL(T val) : value(val), next(nullptr), prev(nullptr) {};
+        DLL(std::initializer_list<T> init);
+};
