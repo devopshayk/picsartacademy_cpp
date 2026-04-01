e31a40a39cbd59f3afca947e5b38931cc1401729
Hayk Poghosyan <hayk@imacs-MacBook-Air.local>
2026-04-01 21:03:49 +0400

    last-updates

---
+++ Core Mastery/18. File Mini Task/task1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    FILE* file = fopen("text.txt", "r");
+
+    if (file == NULL) {
+        printf("File not opened\n");
+        return 1;
+    }
+
+    char buffer[256];
+    int line = 1;
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("%d: %s", line, buffer);
+        line++;
+    }
+
+    fclose(file);
+
+    return 0;
+}
